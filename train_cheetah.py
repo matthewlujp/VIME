Fix information gain calculation

train_cheetah.py
@@ -94,10 +94,11 @@ def train(config_file_path: str, save_dir: str, use_vime: bool, device: str):
 
     for episode in pbar:
         o = env.reset()
-        rewards = []
-        info_gains = None
-        sample_buffer = []
+        rewards, curiosity_rewards = [], []
+        info_gains = []
+        log_likelihoods = []
         q1_losses, q2_losses, policy_losses, alpha_losses, alphas = [],[],[],[],[]
+        H = vime.calc_hessian()
 
         for t in range(env._max_episode_steps):
             if len(memory) < conf.random_sample_num:
@@ -108,7 +109,18 @@ def train(config_file_path: str, save_dir: str, use_vime: bool, device: str):
             o_next, r, done, _ = env.step(a)
             done = False if t == env._max_episode_steps - 1 else bool(done)  # done should be False if an episode is terminated forcefully
             rewards.append(r)
-            sample_buffer.append((o, a, r, o_next, done))
+
+            if use_vime and len(memory) >= conf.random_sample_num:
+                # Calculate curiosity reward in VIME
+                info_gain, log_likelihood = vime.calc_info_gain(o, a, o_next, H)
+                assert not np.isnan(info_gain).any() and not np.isinf(info_gain).any(), "invalid information gain, {}".format(info_gains)
+                info_gains.append(info_gain)
+                log_likelihoods.append(log_likelihood)
+                vime.memorize_episodic_info_gains(info_gain)            
+                r = vime.calc_curiosity_reward(r, info_gain)
+            curiosity_rewards.append(r)
+
+            memory.append(o, a, r, o_next, done)
             o = o_next
 
             # Update agent
@@ -126,25 +138,14 @@ def train(config_file_path: str, save_dir: str, use_vime: bool, device: str):
             if done:
                 break
 
-        # Calculate curiosity reward
-        if use_vime and len(memory) >= conf.random_sample_num:
-            # Calculate curiosity reward in VIME
-            o_batch, a_batch, _, o_next_batch, _ = zip(*sample_buffer)
-            info_gains, log_likelihoods = vime.calc_info_gain(o_batch, a_batch, o_next_batch)
-            assert not np.isnan(info_gains).any() and not np.isinf(info_gains).any(), "invalid information gain, {}".format(info_gains)
-            curiosity_rewards = vime.calc_curiosity_reward(np.array(rewards), info_gains)
-        else:
-            log_likelihoods = np.ones_like(rewards) * -np.inf
-            curiosity_rewards = rewards
-        # Push collected samples into replay buffer
-        for (o, a, _, o_next, done), cr in zip(sample_buffer, curiosity_rewards):
-            memory.append(o, a, cr, o_next, done)
+        if len(log_likelihoods) == 0:
+            log_likelihoods.append(-np.inf)
 
         # Display performance
         episodic_reward = np.sum(rewards)
         reward_moving_avg = episodic_reward if reward_moving_avg is None else (1-moving_avg_coef) * reward_moving_avg + moving_avg_coef * episodic_reward
         if use_vime:
-            pbar.set_description("EPISODE {}, TOTAL STEPS {}, SAMPLES {} --- Steps {}, Curiosity {:.2E}, Rwd {:.1f} (m.avg {:.1f}), Likelihood {:.1f}".format(
+            pbar.set_description("EPISODE {}, TOTAL STEPS {}, SAMPLES {} --- Steps {}, Curiosity {:.1f}, Rwd {:.1f} (m.avg {:.1f}), Likelihood {:.1f}".format(
                 episode, memory.step, len(memory), len(rewards), np.sum(curiosity_rewards), episodic_reward, reward_moving_avg, np.mean(np.exp(log_likelihoods))))
         else:
             pbar.set_description("EPISODE {}, TOTAL STEPS {}, SAMPLES {} --- Steps {}, Rwd {:.1f} (mov avg {:.1f})".format(
@@ -175,12 +176,10 @@ def train(config_file_path: str, save_dir: str, use_vime: bool, device: str):
             for _ in range(conf.vime_update_per_episode):
                 elbo = vime.update_posterior(memory)
             metrics['ELBO'].append(elbo)
+            metrics['D_KL_median'].append(np.median(info_gains))
+            metrics['D_KL_mean'].append(np.mean(info_gains))
             lineplot(metrics['episode'][-len(metrics['ELBO']):], metrics['ELBO'], 'ELBO', log_dir)
-            if info_gains is not None:
-                vime.memorize_episodic_info_gains(info_gains)            
-                metrics['D_KL_median'].append(np.median(info_gains))
-                metrics['D_KL_mean'].append(np.mean(info_gains))
-                multiple_lineplot(metrics['episode'][-len(metrics['D_KL_median']):], np.array([metrics['D_KL_median'], metrics['D_KL_mean']]).T, 'D_KL', ['median', 'mean'], log_dir)
+            multiple_lineplot(metrics['episode'][-len(metrics['D_KL_median']):], np.array([metrics['D_KL_median'], metrics['D_KL_mean']]).T, 'D_KL', ['median', 'mean'], log_dir)
 
         
 

vime.py
@@ -54,40 +54,29 @@ class VIME(nn.Module):
         """
         self._prev_D_KL_medians.append(np.median(info_gains))
 
-    def calc_info_gain(self, s, a, s_next):
+    def calc_info_gain(self, s, a, s_next, H):
         """Calculate information gain D_KL[ q( /cdot | \phi') || q( /cdot | \phi_n) ].
 
         Return info_gain, log-likelihood of each sample \log p(s_{t+1}, a_t, s_)
         """
-        sample_num = len(s)
         self._dynamics_model.set_params(self._params_mu, self._params_rho) # necessary to calculate new gradient
         ll = self._dynamics_model.calc_log_likelihood(
-            torch.tensor(s_next, dtype=torch.float32).to(self._device),
-            torch.tensor(s, dtype=torch.float32).to(self._device),
-            torch.tensor(a, dtype=torch.float32).to(self._device))
-        # assert ll.size() == torch.Size([sample_num]), ll
-        l = -ll
+            torch.tensor(s_next, dtype=torch.float32).to(self._device).unsqueeze(0),
+            torch.tensor(s, dtype=torch.float32).to(self._device).unsqueeze(0),
+            torch.tensor(a, dtype=torch.float32).to(self._device).unsqueeze(0))
+        l = - ll.mean()
 
-        # Calculate nablas for each sample
-        nablas = []
-        for i in range(sample_num):
-            self._optim.zero_grad()
-            l[i].backward(retain_graph=True)  # Calculate gradient \nabla_\phi l ( = \nalba_\phi -E_{\theta \sim q(\cdot | \phi)}[ \log p(s_{t+1} | \s_t, a_t, \theta) ] )
-            nabla = torch.cat([self._params_mu.grad.data, self._params_rho.grad.data])
-            nablas.append(nabla)
-        nablas = torch.stack(nablas)
-        # assert nablas.size() == torch.Size([sample_num, len(self._params_mu) + len(self._params_rho)]), nablas.size()
+        self._optim.zero_grad()
+        l.backward()  # Calculate gradient \nabla_\phi l ( = \nalba_\phi -E_{\theta \sim q(\cdot | \phi)}[ \log p(s_{t+1} | \s_t, a_t, \theta) ] )
+        nabla = torch.cat([self._params_mu.grad.data, self._params_rho.grad.data])
 
-        H = self._calc_hessian().repeat([sample_num, 1])  # [sample_num x parameter_num]
-        # assert H.size() == torch.Size([sample_num, len(self._params_mu) + len(self._params_rho)]), H.size()
-        
         # \frac{\lambda^2}{2} (\nabla_\phi l)^{\rm T} H^{-1} (\nabla_\phi^{\rm T} l)
         with torch.no_grad():
-            info_gain = .5 * self._lamb ** 2 * torch.sum(nablas.pow(2) * H.pow(-1), dim=1)
+            info_gain = .5 * self._lamb ** 2 * torch.sum(nabla.pow(2) * H.pow(-1))
             # assert not torch.isinf(info_gain).any(), info_gain
-        return info_gain.cpu().numpy(), ll.detach().cpu().numpy()
+        return info_gain.cpu().item(), ll.mean().detach().cpu().item()
 
-    def _calc_hessian(self):
+    def calc_hessian(self):
         """Return diagonal elements of H = [ \frac{\partial^2 l_{D_{KL}}}{{\partial \phi_j}^2} ]_j
 
         \frac{\partial^2 l_{D_{KL}}}{{\partial \mu_j}^2} = - \frac{1}{\log^2 (1 + e^{\phi_j})}
@@ -99,8 +88,14 @@ class VIME(nn.Module):
             H_mu = log_denomi.pow(-2)
             H_rho = 2 * torch.exp(2 * self._params_rho) / (denomi * log_denomi).pow(2)
             H = torch.cat([H_mu, H_rho])
-        assert not torch.isnan(H).any() and not torch.isinf(H).any(), H
         return H
+
+    def _calc_div_kl(self, prev_mu, prev_var):
+        """Calculate D_{KL} [ q(\cdot | \phi) || q(\cdot | \phi_n) ]
+        = \frac{1}{2} \sum^d_i [ \log(var_{ni}) - \log(var_i) + \frac{var_i}{var_{ni}} + \frac{(\mu_i - \mu_{ni})^2}{var_{ni}} ] - \frac{d}{2}
+        """
+        var = (1 + self._params_rho.exp()).log().pow(2)
+        return .5 * ( prev_var.log() - var.log() + var / prev_var + (self._params_mu - prev_mu).pow(2) / prev_var ).sum() - .5 * len(self._params_mu)
 
     def update_posterior(self, memory):
         """
@@ -134,13 +129,6 @@ class VIME(nn.Module):
             self._dynamics_model.set_params(self._params_mu, self._params_rho)
 
         return elbo.item()
-
-    def _calc_div_kl(self, prev_mu, prev_var):
-        """Calculate D_{KL} [ q(\cdot | \phi) || q(\cdot | \phi_n) ]
-        = \frac{1}{2} \sum^d_i [ \log(var_{ni}) - \log(var_i) + \frac{var_i}{var_{ni}} + \frac{(\mu_i - \mu_{ni})^2}{var_{ni}} ] - \frac{d}{2}
-        """
-        var = (1 + self._params_rho.exp()).log().pow(2)
-        return .5 * ( prev_var.log() - var.log() + var / prev_var + (self._params_mu - prev_mu).pow(2) / prev_var ).sum() - .5 * len(self._params_mu)
 
     def state_dict(self):
         return {